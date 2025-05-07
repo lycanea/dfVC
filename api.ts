91fcb2bcebb2d953c4dc3539fdd4882c89a9581a
lycanea <lycanea@lycanea.dev>
2025-05-07 20:13:56 +1000

    make the init endpoint generate a token for each plot

--- api.ts
+++ api.ts
@@ -1,4 +1,7 @@
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
 const validDFIps = ['51.222.245.229'];
+let tokens = {} // uhh just an in memory token thingy nothing to see here... this ruins the point of jwt
 
 let endpoints = {
 	// okay so input will be like token&asd=123,123,123&silly=456,456,456&meow=27,6,3
@@ -9,13 +12,17 @@ let endpoints = {
 	"/api/v1/init": (req: Request) => {
 		const headers = req.headers.toJSON()
 		if (!headers['x-forwarded-for'] || !validDFIps.includes(headers['x-forwarded-for']) || !headers['user-agent']) return new Response("Unauthorised");
-
+		// should be secure if behind a proxy
 		console.log(headers)
 
 		const matches = headers['user-agent'].match(/Hypercube\/([\d.]+) \((\d+), (.*)\)/) || [];
 		const plotId = matches[2];
-
-		return new Response(`Hello, ${plotId}!`);
+		
+		const secretKey = process.env.JWT_SECRET;
+		if (!secretKey || !plotId) return new Response("some serverside error")
+		const token = jwt.sign({ id: plotId }, secretKey, {expiresIn: "14d"}); // generate the token for the plotId (should maybe have expiration or secondary validation)
+		token[plotId] = token
+		return new Response(`${token}|1`); // `token|latest api version` easily like... usable format on dfside
 	},
 }
 
