e2e522d42601d1537d5b56343adbc5f741e3572b
Lycanea <lycanea@lycanea.dev>
2025-05-09 13:30:30 +1000

    do more stuff

--- api.ts
+++ api.ts
@@ -19,10 +19,10 @@ type Rooms = {
 }
 interface Api {
 	rooms: Rooms;
-	counter: number;
 	endpoints: any;
-	setCounter(newCount: number): void;
-	newRoom(roomId: string): void;
+	newRoom(roomId: string): {};
+	newUser(roomId: string, userId: string): {};
+	updateUserPosition(roomId: string, user: string, position: {}): void;
 }
 
 const validDFIps = ['51.222.245.229'];
@@ -35,15 +35,20 @@ let endpoints = {
 	"/api/v1/update/:input": (req: Request & {params: {input: string}}) => {
 		const headers = req.headers.toJSON()
 		if (!headers['x-forwarded-for'] || !validDFIps.includes(headers['x-forwarded-for'])) return new Response("Unauthorised");
+		//request from df
 		const splitInput: string[] = req.params.input.split('&')
 		const token: string | undefined = splitInput[0]
 		if (!token) return new Response("Unauthorised");
+		//request with a token
 		const tokenValidation: JwtPayload | string = jwt.verify(token, jwt_token);
 		if (typeof tokenValidation === 'string' || tokenValidation instanceof String) return new Response("error parsing token");
+		// request with valid valid
 		const plotId: string = tokenValidation.id
 		if (!(token == tokens[plotId])) return new Response("Unauthorised"); // if token isnt valid and isnt in the tokens object then yk... die
+		// request with valid and correct token
 		delete splitInput[0]
 		
+		// iterate over all the players in the request
 		const decoded: {[key: string]: {x: number, y: number, z: number}}  = {}
 		splitInput.forEach(function(value: string){
 			const splitUser: string | undefined = value.split('=')[0]
@@ -51,14 +56,11 @@ let endpoints = {
 			const userData: string[] | undefined = value.split('=')[1]?.split(',')
 			if (!userData || !userData[0] || !userData[1] || !userData[2]) return; // the... other check thing i fucking hate typescript maybe idk there has to be a better way to do this, this has like 100 if statement checks for not undefined in it so far
 			decoded[splitUser] = {x: parseFloat(userData[0]), y: parseFloat(userData[1]), z: parseFloat(userData[2])}
+			api.updateUserPosition(plotId, splitUser, {x: parseFloat(userData[0]), y: parseFloat(userData[1]), z: parseFloat(userData[2])})
 		});
 		
 		console.log(`update req from ${plotId}`)
 		console.log(decoded)
-		return new Response();
-	},
-	"/api/increment": (req: Request) => {
-		api.setCounter(api.counter + 1)
 		return new Response();
 	},
 	"/api/v1/init": (req: Request) => {
@@ -81,13 +83,19 @@ let endpoints = {
 
 const api: Api = {
 	rooms: {"asd": {"asd": {"connected": false}}},
-	counter: 0,
 	endpoints: endpoints,
-	setCounter (newCount) {
-		api.counter = newCount
-	},
-	newRoom (roomId) {
+	newRoom (roomId: string): {} {
 		api.rooms[roomId] = {}
+		return api.rooms[roomId];
+	},
+	newUser (roomId: string, userId: string): {} {
+		let targetRoom = api.rooms[roomId]; if (!targetRoom) targetRoom = api.newRoom(roomId);
+		targetRoom[userId] = {"connected": false};
+		return targetRoom[userId]
+	},
+	updateUserPosition (roomId: string, user: string, position: {x: number, y: number, z: number}) {
+		let targetRoom = api.rooms[roomId]; if (!targetRoom) targetRoom = api.newRoom(roomId); //gets room, creates if non existant
+		let targetUser = targetRoom[user]; if (!targetUser) targetUser = api.newUser(roomId, user);
 	}
 }
 
