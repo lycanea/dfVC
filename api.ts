17ebf6f7744b5a8dd528d2477b19f4a9fc3e753f
Lycanea <lycanea@lycanea.dev>
2025-05-09 16:02:40 +1000

    stop uselessly storing all the "decoded data" in a variable

--- api.ts
+++ api.ts
@@ -29,7 +29,6 @@ const jwt_token: string | undefined = process.env.JWT_SECRET
 if (!jwt_token) {console.warn("invalid JWT_SECRET");process.abort();}
 
 let endpoints = {
-	// okay so input will be like token&asd=123,123,123&silly=456,456,456&meow=27,6,3
 	"/api/v1/update/:input": (req: Request & {params: {input: string}}) => {
 		const headers = req.headers.toJSON()
 		if (!headers['x-forwarded-for'] || !validDFIps.includes(headers['x-forwarded-for'])) return new Response("Unauthorised");
@@ -47,18 +46,19 @@ let endpoints = {
 		delete splitInput[0]
 		
 		// iterate over all the players in the request
-		const decoded: {[key: string]: {x: number, y: number, z: number}}  = {}
 		splitInput.forEach(function(value: string){
+			// each input has an id so some can be excluded?
+			// maybe stupid idk, just make the data efficient enough thats its okay to send it all over every time
+			// userUUID=1:x,2:y,3:z,4:name,5:canTalk,6:teamId
+			// asd     =1:1,2:2,3:3,4:asd ,5:1      ,6:2763
 			const splitUser: string | undefined = value.split('=')[0]
 			if (!splitUser) return; //if no user... somehow, then uhhh skip this index (appease the typescript gods)
 			const userData: string[] | undefined = value.split('=')[1]?.split(',')
 			if (!userData || !userData[0] || !userData[1] || !userData[2]) return; // the... other check thing i fucking hate typescript maybe idk there has to be a better way to do this, this has like 100 if statement checks for not undefined in it so far
-			decoded[splitUser] = {x: parseFloat(userData[0]), y: parseFloat(userData[1]), z: parseFloat(userData[2])}
 			api.updateUserPosition(plotId, splitUser, {x: parseFloat(userData[0]), y: parseFloat(userData[1]), z: parseFloat(userData[2])})
 		});
 		
 		console.log(`update req from ${plotId}`)
-		console.log(decoded)
 		return new Response();
 	},
 	"/api/v1/init": (req: Request) => {
